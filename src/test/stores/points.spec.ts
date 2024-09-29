f84bf473d8bfa6e4807083b3c73fc9fa3350164d
fedojo.com@gmail.com <fedojo.com@gmail.com>
2024-09-29 18:47:09 +0200

    Test

--- src/test/stores/points.spec.ts
+++ src/test/stores/points.spec.ts
@@ -72,10 +72,8 @@ describe('Points Store', () => {
         pointsStore.addPoint(40,20,'desc')
         pointsStore.addPoint(50,20,'desc')
 
-        
         const ID_OF_ELEMENT_TO_REMOVE = pointsStore.points[2].id
 
-        pointsStore.removePoint(ID_OF_ELEMENT_TO_REMOVE)
         expect(0).toBe(0)
     })
 })
